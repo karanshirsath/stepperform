2d59dd7aab7d8cb388dfcf08bf1cab704c0f429e
Naveen Gayar <naveen_gayar@infosys.com>
2020-05-15 21:48:44 +0530

    imported angular material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import{MatStepperModule} from '@angular/material/stepper';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+import { MatCardModule} from '@angular/material/card';
+import { MatSelectModule} from '@angular/material/select';
+import { constants } from 'buffer';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatGridListModule} from '@angular/material/grid-list';
 import { StepperComponent } from './stepper/stepper.component';
 
 @NgModule({
@@ -14,7 +23,18 @@ import { StepperComponent } from './stepper/stepper.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatGridListModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
