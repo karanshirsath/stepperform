731e842a25390496eebd1bdd04d78dd92b296699
Naveen Gayar <naveen_gayar@infosys.com>
2020-05-15 21:52:10 +0530

    added a routing for stepper form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StepperComponent } from './stepper/stepper.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'form', component: StepperComponent },
+{ path: '**', redirectTo: '/form', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
