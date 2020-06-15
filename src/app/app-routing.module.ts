c62c6ac1ab1db3e6e90cffd1c8dbaea1c6026082
KARAN <karan@KARANs-MacBook-Air.local>
2020-06-15 19:13:08 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TabComponent } from './tab/tab.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 const routes: Routes = [
   { path: 'form', component: TabComponent },
+  { path: 'signIn', component: LoginComponent },
 { path: '**', redirectTo: '/form', pathMatch: 'full' }
 ];
 
