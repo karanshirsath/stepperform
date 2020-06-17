71ef728ffabf254d198ee38e6ee37008b0d8358f
Swapnil Verma <me@infosys.com>
2020-06-17 13:41:06 +0530

    vInfo changes

--- src/app/components/v-info/v-info.component.ts
+++ src/app/components/v-info/v-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output,EventEmitter,ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import {StepperComponent} from 'src/app/stepper/stepper.component'
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,16 +13,17 @@ export class VInfoComponent implements OnInit {
   @Output() OnRegister= new EventEmitter()
   @Output() OnToggle=new EventEmitter()
  vehicleInfoForm:FormGroup;
-  constructor(private formBuilder:FormBuilder) { } 
+  constructor(private router: Router, private formBuilder:FormBuilder) { } 
 
   ngOnInit(): void {
     this.vehicleInfoForm=this.formBuilder.group({
       registrationNumber:['',[Validators.required,Validators.minLength(8),Validators.maxLength(8)]],
       certificateNumber:['',[Validators.required,Validators.minLength(9),Validators.maxLength(9)]],
       purpose:['',Validators.required],
-      ysNo:['']
+      ysNo:['',Validators.required]
     })}
     submit=() => {
+      //this.router.navigate(['./insuring-party.component']);
       console.log(this.vehicleInfoForm.value,"vehicle Info");
       this.OnRegister.emit(this.vehicleInfoForm.value);
 
