25a199d89c57e7c12fedfc691f5750896dee77ff
Sanket Jain <sanket10.knp@gmail.com>
2020-06-17 18:19:23 +0530

    Modified Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../src/**/*.stories.(ts|mdx)'],
-  addons: ['@storybook/addon-actions', 
+  addons: ['@storybook/addon-storysource',
+           '@storybook/addon-actions', 
            '@storybook/addon-links', 
            '@storybook/addon-notes',
            '@storybook/addon-docs'],
