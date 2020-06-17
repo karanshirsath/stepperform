d6372f718ee39e57ae25b028a8b039f9365a1b98
karthickp <karthick111094@gmail.com>
2020-06-17 20:43:33 +0530

    common declartaion in preview.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../src/**/*.stories.(ts|mdx)'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes','@storybook/addon-docs'],
 };
