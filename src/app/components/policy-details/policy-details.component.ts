af4d6362ede4072d60c7bbc5ada3a5bcb920e688
samyak1902 <60135154+samyak1902@users.noreply.github.com>
2020-06-19 16:12:30 +0530

    +policy details validation acc to excel doc

--- src/app/components/policy-details/policy-details.component.ts
+++ src/app/components/policy-details/policy-details.component.ts
@@ -11,7 +11,10 @@ export class PolicyDetailsComponent implements OnInit {
   @Output() OnRegister = new EventEmitter()
   @Output() OnToggle = new EventEmitter()
   registerForm: FormGroup;
-
+  todaydate=new Date()
+  day=''
+  month=''
+  mindate=""
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
@@ -21,6 +24,21 @@ export class PolicyDetailsComponent implements OnInit {
       check1: ['', Validators.requiredTrue],
       check2: ['', Validators.requiredTrue]
     });
+    this.todaydate.setDate(this.todaydate.getDate() + 1);
+    console.log(this.todaydate);
+    if((this.todaydate.getMonth()+1)<10){
+      this.month="0"+(this.todaydate.getMonth()+1)
+    }else{
+      this.month=""+(this.todaydate.getMonth()+1)
+    }
+    if(this.todaydate.getDate()<10){
+      this.day="0"+this.todaydate.getDate()
+    }else{
+      this.day=""+this.todaydate.getDate()
+    }
+    this.mindate=""+this.todaydate.getFullYear()+"-"+this.month+"-"+this.day
+    console.log(this.mindate);
+    
   }
   submit=()=>{
     console.log(this.registerForm.value, "policy details");
